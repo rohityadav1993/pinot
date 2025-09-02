ae154e35169e79c78cf00b270b3d886bc7192bf7
tarun11Mavani <35224468+tarun11Mavani@users.noreply.github.com>
2025-09-03 00:10:41 +0530

    [clean cherry-pick] Commit time compaction for upsert tables (#16344)
    Summary:
    Clean cherry-pick of [[ https://github.com/apache/pinot/pull/16344 |#16344 ]] to land onto release-1.4.x
    
    Test Plan: PRR: https://docs.google.com/document/d/1x8g9QTg0Hc-3bes6ItVSRU-4av1o4kIgjz8IdTN5lCo/edit?tab=t.egedwyc1tgi#bookmark=id.ejap8fbo0seo
    
    Reviewers: #ldap_rta-group, ureview, rohity
    
    Reviewed By: #ldap_rta-group, rohity
    
    Tags: #has_java
    
    JIRA Issues: EVA-12130
    
    Differential Revision: https://code.uberinternal.com/D19784165

--- pinot-segment-local/src/main/java/org/apache/pinot/segment/local/indexsegment/mutable/MutableSegmentImpl.java
+++ pinot-segment-local/src/main/java/org/apache/pinot/segment/local/indexsegment/mutable/MutableSegmentImpl.java
@@ -1171,6 +1171,11 @@ public class MutableSegmentImpl implements MutableSegment {
   }
 
   @Nullable
+  public String getDeleteRecordColumn() {
+    return _deleteRecordColumn;
+  }
+
+  @Nullable
   @Override
   public ThreadSafeMutableRoaringBitmap getQueryableDocIds() {
     return _queryableDocIds;
